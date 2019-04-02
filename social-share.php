4cbe04f76c95452253e0d69c18646430a6567d02
Lukman Nakib <lukman.nakib>
2019-04-02 16:37:55 +0600

    static port folio
    project link to the 000webhost

---
+++ social-share.php
@@ -0,0 +1,20 @@
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.css" />
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-flat.css" />
+<div id="share"></div>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.min.js"></script>
+<script>		
+    //social share
+  	jQuery(document).ready(function($) {
+  	    
+  	   $("#share").jsSocials({
+        shareIn: "popup",
+        showCount: true,
+	    showLabel: false,
+	            shares: [  "facebook", "twitter","linkedin", "pinterest","whatsapp"]
+	        });
+  	    
+  		
+  	});
+
+
+</script>		
